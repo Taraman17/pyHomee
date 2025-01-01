9269a6ff980701623866ec83a79fc341f4d89d09
Taraman17 <mozilla@adrario.de>
2025-01-01 17:25:23 +0000

    fix: add py.typed and bump min python to 3.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 __version__ = "1.0.1"
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -19,5 +19,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.11",
 )
